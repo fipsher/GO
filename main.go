7492ae3108ae3e68e66c768f7466e43779244d35
AndrewThe <fipsher@gmail.com>
2018-12-12 23:42:26 +0200

    make it better

--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-func main() {
+func runLocal() {
+	reader := FileLinSysReader{"system.txt"}
+	//reader := JSONLinSysReader{"[[2,3],[3,5]]", "[8,13]"}
+	x := readAndSolve(reader)
+	printVector("result", x)
+}
+
+func runServer() {
 	port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	initHandlers()
+	initCacher()
+	fmt.Println("Starting Cholesky solving server on port 8080...")
+	fmt.Println("Visit / for GUI")
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/solve", solveHandler)
+	http.HandleFunc("/start", startHandler)
+	http.HandleFunc("/stop/", stopHandler)
+	http.HandleFunc("/result/", resultHandler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("static/*.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
-
-	router.Run(":" + port)
+func main() {
+	//runLocal()
+	runServer()
 }
