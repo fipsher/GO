e20e7fa2f1fd22a294b9c98e9f7cdf894583c187
AndrewThe <fipsher@gmail.com>
2018-12-12 23:01:56 +0200

    make it better

--- src/main.go
+++ src/main.go
@@ -14,16 +14,16 @@ func runLocal() {
 }
 
 func runServer() {
-	initHandlers()
-	initCacher()
+	// initHandlers()
+	// initCacher()
 	fmt.Println("Starting Cholesky solving server on port 8080...")
 	fmt.Println("Visit / for GUI")
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
-	http.HandleFunc("/solve", solveHandler)
-	http.HandleFunc("/start", startHandler)
-	http.HandleFunc("/stop/", stopHandler)
-	http.HandleFunc("/result/", resultHandler)
+	// http.HandleFunc("/solve", solveHandler)
+	// http.HandleFunc("/start", startHandler)
+	// http.HandleFunc("/stop/", stopHandler)
+	// http.HandleFunc("/result/", resultHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
