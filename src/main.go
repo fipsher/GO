96e0612104f69d4cd7fbe79573c28ff5942d06e1
AndrewThe <fipsher@gmail.com>
2018-12-12 23:24:43 +0200

    make it better

--- src/main.go
+++ src/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
